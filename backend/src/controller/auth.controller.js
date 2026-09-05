4f4bde3bd27cf145971c2a8288be73f4f909e67f
Krish-AryanDev <krisharyan.dev@gmail.com>
2026-09-05 23:22:24 +0530

    add complete auth feature

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -2,39 +2,39 @@ const farmerModel = require("../models/farmer.model")
 const otpModel = require("../models/otp.model")
 const jwt = require("jsonwebtoken")
 
-{/*  This function generates OTP*/}
+{/*  This function generates OTP*/ }
 
 const generateOTP = () => {
     return Math.floor(1000 + Math.random() * 9000).toString(); // Generates a 4-digit OTP
 }
 
-{/* This function is connected to to /register route and checks if the userData is present in the database and sends them OTP for verification*/}
+{/* This function is connected to to /register route and checks if the userData is present in the database and sends them OTP for verification*/ }
 
 const registerFarmer = async (req, res) => {
-    try{
+    try {
         const { userData } = req.body /* userData can be farmerID or AadharNumber */
 
-        if(!userData){
+        if (!userData) {
             return res.status(400).json({
                 message: "userData is required"
             })
         }
 
         let existingFarmer = null;
-        if(/^\d{11}$/.test(userData)){
+        if (/^\d{11}$/.test(userData)) {
             // farmerID
             existingFarmer = await farmerModel.findOne({ farmerID: userData })
 
-        }else if(/^\d{12}$/.test(userData)){
+        } else if (/^\d{12}$/.test(userData)) {
             // AadharNumber
             existingFarmer = await farmerModel.findOne({ AadharNumber: userData })
-        }else {
+        } else {
             return res.status(400).json({
                 message: "Invalid userData format. It should be either 11-digit farmerID or 12-digit AadharNumber."
             })
         }
 
-        if(existingFarmer){
+        if (existingFarmer) {
 
             const phoneNumber = existingFarmer.phoneNumber;
             const otp = generateOTP();
@@ -54,19 +54,18 @@ const registerFarmer = async (req, res) => {
 
             /*await smsProvider.send(phoneNumber, `Your OTP is: ${otp}. It will expire in 5 minutes.`); */
 
-            
-
             return res.status(200).json({
                 message: "otp sent successfully",
-                present: true
+                present: true,
+                phoneNumber: existingFarmer.phoneNumber
             })
-        }else{
+        } else {
             return res.status(200).json({
                 message: "Farmer is not present in the database",
                 present: false
             })
         }
-    }catch(e) {
+    } catch (e) {
         res.status(500).json({
             message: "Error registering farmer",
             error: e.message
@@ -74,29 +73,29 @@ const registerFarmer = async (req, res) => {
     }
 }
 
-{/* This function is connected to /verify-otp route and verifies OTP*/}
+{/* This function is connected to /verify-otp route and verifies OTP*/ }
 
 const verifyOTP = async (req, res) => {
-    try{
+    try {
         const { phoneNumber, otp } = req.body
 
-        if(!phoneNumber || !otp){
+        if (!phoneNumber || !otp) {
             return res.status(400).json({
                 message: "phoneNumber and otp are required",
                 success: false
             })
         }
 
-        const existingOTP = await otpModel.findOne({phoneNumber, otp})
+        const existingOTP = await otpModel.findOne({ phoneNumber, otp })
 
-        if(!existingOTP){
+        if (!existingOTP) {
             return res.status(400).json({
                 message: "OTP is invalid or expired",
                 success: false
             })
         }
 
-        await otpModel.deleteOne({phoneNumber, otp}) // Delete the OTP after successful verification
+        await otpModel.deleteOne({ phoneNumber, otp }) // Delete the OTP after successful verification
 
         // Find the farmer associated with this phone number
         const farmer = await farmerModel.findOne({ phoneNumber });
@@ -133,7 +132,7 @@ const verifyOTP = async (req, res) => {
             }
         })
 
-    }catch (e) {
+    } catch (e) {
         res.status(500).json({
             message: "Error verifying OTP",
             error: e.message
