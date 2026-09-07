5859829cb10a2d22d84f4f0b5562bbcbe3d07c0a
Pranesh <pranesh2421@gmail.com>
2026-09-07 15:13:34 +0530

    add middleware

--- backend/src/client/routes/slot_book.routes.js
+++ backend/src/client/routes/slot_book.routes.js
@@ -3,8 +3,8 @@ const authMiddleware = require("../middlewares/auth.middleware")
 const router = express.Router()
 const { slot , check_availability,book} = require("../controller/slot.controller")
 
-router.get("/slot",slot);
-router.get("/can_book",check_availability);
-router.post("/book",book);
+router.get("/slot",authMiddleware,slot);
+router.get("/can_book",authMiddleware,check_availability);
+router.post("/book",authMiddleware,book);
 
 module.exports = router;
